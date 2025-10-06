32b0b2f54d6c7024a863bacafbbaef588a65b640
Henry Brown <henry.brown321@gmail.com>
2025-10-06 20:46:02 +0100

    Revert "sandbox: phase 1,2,3"
    This reverts commit 47f1d4d4a719c2687fc0ed6f994e5ae98c996459.

--- src/animations/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Public exports for the WAAPI animation system
- */
-
-export { createWebAnimationEngine } from './web-animation-engine';
-export type { WebAnimationEngine } from './web-animation-engine';
-export type { AnimationDefinition, AnimationMetadata } from './animation-types';
